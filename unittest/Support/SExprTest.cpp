c572fb455aba0956be35c73a37978c00ae1ae7a9
Gyula Sallai <salla016@gmail.com>
2020-04-03 21:46:30 +0200

    Updated SExprTest

--- unittest/Support/SExprTest.cpp
+++ unittest/Support/SExprTest.cpp
@@ -22,34 +22,34 @@
 
 using namespace gazer;
 
+namespace
+{
+
 TEST(SExprTest, TestParse)
 {
-    std::unique_ptr<sexpr::Value> expected;
-
-    expected.reset(sexpr::list({
+    EXPECT_EQ(*sexpr::parse("(A B\n \nC)"), *std::unique_ptr<sexpr::Value>(sexpr::list({
         sexpr::atom("A"),
         sexpr::atom("B"),
         sexpr::atom("C")
-    }));
-    EXPECT_EQ(*sexpr::parse("(A B\n \nC)"), *expected);
+    })));
 
-    expected.reset(sexpr::list({
+    EXPECT_EQ(*sexpr::parse("(A (X Y Z))"), *std::unique_ptr<sexpr::Value>(sexpr::list({
         sexpr::atom("A"),
         sexpr::list({
             sexpr::atom("X"),
             sexpr::atom("Y"),
             sexpr::atom("Z")
         })
-    }));
-    EXPECT_EQ(*sexpr::parse("(A (X Y Z))"), *expected);
+    })));
 
-    expected.reset(sexpr::list({
+    EXPECT_EQ(*sexpr::parse("(A (X Y (Z)))"), *std::unique_ptr<sexpr::Value>(sexpr::list({
         sexpr::atom("A"),
         sexpr::list({
             sexpr::atom("X"),
             sexpr::atom("Y"),
             sexpr::list({ sexpr::atom("Z") })
         })
-    }));
-    EXPECT_EQ(*sexpr::parse("(A (X Y (Z)))"), *expected);
+    })));
 }
+
+} // namespace
