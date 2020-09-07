84352b9bc6d24a2137d6a9afa4cf5f71fb69e588
Mihály Dobos-Kovács <dkmisu@gmail.com>
2020-09-07 22:31:17 +0200

    Add bitvectors to theta type converter

---
+++ tools/gazer-theta/lib/ThetaType.h
@@ -0,0 +1,39 @@
+//==-------------------------------------------------------------*- C++ -*--==//
+//
+// Copyright 2019 Contributors to the Gazer project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+#ifndef GAZER_TOOLS_GAZERTHETA_LIB_THETATYPE_H
+#define GAZER_TOOLS_GAZERTHETA_LIB_THETATYPE_H
+
+#include "gazer/Core/Type.h"
+
+#include <string>
+
+namespace gazer::theta
+{
+
+class ThetaType final
+{
+public:
+    ThetaType() = delete;
+
+    static std::string getThetaTypeName(Type& type);
+    static std::string getThetaTypeDefaultValue(Type& type);
+};
+
+} // end namespace gazer::theta
+
+#endif // GAZER_TOOLS_GAZERTHETA_LIB_THETATYPE_H
