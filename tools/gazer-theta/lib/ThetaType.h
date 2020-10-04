c3912bc98a04273c3129045a2ef4850dfcd5d243
Mihály Dobos-Kovács <dkmisu@gmail.com>
2020-10-04 16:40:47 +0200

    Refactor ThetaType

--- tools/gazer-theta/lib/ThetaType.h
+++ tools/gazer-theta/lib/ThetaType.h
@@ -25,8 +25,71 @@
 namespace gazer::theta
 {
 
-std::string thetaType(Type& type);
-std::string defaultValueForType(Type& type);
+inline std::string thetaType(const Type &type)
+{
+    switch (type.getTypeID()) {
+    case Type::IntTypeID:
+        return "int";
+    case Type::RealTypeID:
+        return "rat";
+    case Type::BoolTypeID:
+        return "bool";
+    case Type::ArrayTypeID: {
+        const auto& arrTy = llvm::cast<ArrayType>(type);
+        return "[" + thetaType(arrTy.getIndexType()) + "] -> " + thetaType(arrTy.getElementType());
+    }
+    case Type::BvTypeID: {
+        const auto& bvTy = llvm::cast<BvType>(type);
+        return "bv[" + std::to_string(bvTy.getWidth()) + "]";
+    }
+    default:
+        llvm_unreachable("Types which are unsupported by theta should have been eliminated earlier!");
+    }
+}
+
+inline std::string thetaEscapedType(const Type &type)
+{
+    switch (type.getTypeID()) {
+    case Type::IntTypeID:
+        return "int";
+    case Type::RealTypeID:
+        return "rat";
+    case Type::BoolTypeID:
+        return "bool";
+    case Type::ArrayTypeID: {
+        const auto& arrTy = llvm::cast<ArrayType>(type);
+        return "__" + thetaEscapedType(arrTy.getIndexType()) + "_" + thetaEscapedType(arrTy.getElementType()) + "__";
+    }
+    case Type::BvTypeID: {
+        const auto& bvTy = llvm::cast<BvType>(type);
+        return "bv_" + std::to_string(bvTy.getWidth());
+    }
+    default:
+        llvm_unreachable("Types which are unsupported by theta should have been eliminated earlier!");
+    }
+}
+
+inline std::string defaultValueForType(const Type &type)
+{
+    switch (type.getTypeID()) {
+    case Type::IntTypeID:
+        return "0";
+    case Type::RealTypeID:
+        return "0%1";
+    case Type::BoolTypeID:
+        return "false";
+    case Type::ArrayTypeID: {
+        const auto& arrTy = llvm::cast<ArrayType>(type);
+        return "[<" + thetaType(arrTy.getIndexType()) + ">default <- " + defaultValueForType(arrTy.getElementType()) + "]";
+    }
+    case Type::BvTypeID: {
+        const auto& bvTy = llvm::cast<BvType>(type);
+        return std::to_string(bvTy.getWidth()) + "'d0";
+    }
+    default:
+        llvm_unreachable("Types which are unsupported by theta should have been eliminated earlier!");
+    }
+}
 
 } // end namespace gazer::theta
 
