efa59362515ea3e75f08c83b3eb51bfc0cac2100
Akos Hajdu <hajdua@mit.bme.hu>
2020-12-30 18:31:41 +0100

    Use GH actions for build and tests (#75)

--- test/check-cex.sh
+++ test/check-cex.sh
@@ -6,7 +6,16 @@ do
   clang -c -emit-llvm "$INPUTF" -o "$TEMP_DIR/$(basename $INPUTF).bc"
 done
 
-llvm-link "$TEMP_DIR"/*.bc -o - | lli
+if command -v llvm-link-9 &> /dev/null
+then
+  LLVMLINK="llvm-link-9"
+  LLI="lli-9"
+else
+  LLVMLINK="llvm-link"
+  LLI="lli"
+fi
+
+"$LLVMLINK" "$TEMP_DIR"/*.bc -o - | "$LLI"
 RESULT=$?
 rm -r "$TEMP_DIR"
 exit $RESULT
