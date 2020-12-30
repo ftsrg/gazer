9399aded8df11ed461ffb7f804fbfeb7dff1e8fd
Akos Hajdu <hajdua@mit.bme.hu>
2020-12-30 11:41:16 +0100

    Support different aliases in check-cex script

--- test/check-cex.sh
+++ test/check-cex.sh
@@ -6,7 +6,16 @@ do
   clang -c -emit-llvm "$INPUTF" -o "$TEMP_DIR/$(basename $INPUTF).bc"
 done
 
-llvm-link "$TEMP_DIR"/*.bc -o - | lli
+if command -v llvm-link &> /dev/null
+then
+    LLVMLINK="llvm-link"
+	LLI="lli"
+else
+    LLVMLINK="llvm-link-9"
+	LLI="lli-9"
+fi
+
+eval "$LLVMLINK $TEMP_DIR/*.bc -o - | $LLI"
 RESULT=$?
 rm -r "$TEMP_DIR"
 exit $RESULT
